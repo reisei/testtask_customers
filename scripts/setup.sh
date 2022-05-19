cd09daccad4b1dfc5a93aa39bf438302c24b3d8b
Aleksandr Chapkin <watashi@reisei.ru>
2022-05-19 23:39:51 +0300

    Tweaking setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,3 @@
-docker run -d --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
+minikube addons enable registry 
+minikube addons enable ingress 
+docker run -d --rm -it --name registry-socat --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
